50aaba7c3187b886fc70731bfc3dfa4d15fca50b
julesmuskala <30392252+julesmuskala@users.noreply.github.com>
2024-01-26 22:27:24 +0100

    doc

--- src/rgb_rotate.s
+++ src/rgb_rotate.s
@@ -8,34 +8,33 @@ rotate_pixels:
     mov r8, rcx
     xor rcx, rcx
 
-    movups xmm2, [rdx]              // xmm1 [0_f32, 1_f32, 2_f32, null_f32]
+    movups xmm2, [rdx]                  // xmm1 [idx0_f32, idx1_f32, idx2_f32, null_f32]
     pshufd xmm2, xmm2, 0x6C
 
-    movups xmm3, [rdx + 12]         // xmm2 [3_f32, 4_f32, 5_f32, null_f32]
+    movups xmm3, [rdx + 12]             // xmm2 [idx3_f32, idx4_f32, idx5_f32, null_f32]
     pshufd xmm2, xmm2, 0x6C
 
-    movups xmm4, [rdx + 20]         // xmm3 [6_f32, 7_f32, 8_f32, null_f32]
+    movups xmm4, [rdx + 20]             // xmm3 [idx6_f32, idx7_f32, idx8_f32, null_f32]
     psrldq xmm4, 4              
 
 loop_start:
     cmp rcx, r8
     je loop_end
     
-    xor eax, eax
-    movzx eax, byte ptr [rdi + rcx + 2]
+    movzx eax, byte ptr [rdi + rcx + 2] // move (B) value to eax
     shl eax, 16
-    mov ax, word ptr [rdi + rcx]
+    mov ax, word ptr [rdi + rcx]        // move (R) and (G) value to eax
 
     movd xmm1, eax
-    pmovzxbd xmm1, xmm1
-    cvtdq2ps xmm1, xmm1             // xmm1 [0_f32, 1_f32, 2_f32, null_f32]             
+    pmovzxbd xmm1, xmm1                 // spread u8 to u32
+    cvtdq2ps xmm1, xmm1                 // ints to floats            
 
 
     movaps xmm0, xmm1
     mulps xmm0, xmm2
-    haddps xmm0, xmm0
-    haddps xmm0, xmm0
-    cvttss2si eax, xmm0
+    haddps xmm0, xmm0               
+    haddps xmm0, xmm0                   // sum floats
+    cvttss2si eax, xmm0                 // convert floats to ints
 
     // clamp (0 - 255)
     mov ebx, 255
@@ -45,15 +44,14 @@ loop_start:
     mov ebx, 0
     cmovl eax, ebx
 
-    mov byte ptr [rsi + rcx], al
-    inc rcx
+    mov byte ptr [rsi + rcx], al        // move (R) to target
 
 
     movaps xmm0, xmm1
     mulps xmm0, xmm3
     haddps xmm0, xmm0
-    haddps xmm0, xmm0
-    cvttss2si eax, xmm0
+    haddps xmm0, xmm0                   // sum floats
+    cvtps2pi eax, xmm0                  // convert floats to ints
 
     // clamp (0 - 255)
     mov ebx, 255
@@ -63,15 +61,15 @@ loop_start:
     mov ebx, 0
     cmovl eax, ebx
 
-    mov byte ptr [rsi + rcx], al
-    inc rcx
+    mov byte ptr [rsi + rcx + 1], al    // move (G) to target
 
 
     movaps xmm0, xmm1
     mulps xmm0, xmm4
     haddps xmm0, xmm0
-    haddps xmm0, xmm0
-    cvttss2si eax, xmm0
+    haddps xmm0, xmm0                   // sum floats
+    cvtps2pi eax, xmm0                  // convert floats to ints
+
 
     // clamp (0 - 255)
     mov ebx, 255
@@ -81,10 +79,10 @@ loop_start:
     mov ebx, 0
     cmovl eax, ebx
 
-    mov byte ptr [rsi + rcx], al
-    inc rcx
+    mov byte ptr [rsi + rcx + 2], al    // move (B) to target
 
 
+    add rcx, 3
     jmp loop_start
     
 loop_end:
